f6877b187c99fec61a3b18464b339343b4bf4c0a
Graham Ollis <perl@wdlabs.com>
2019-03-05 16:33:59 -0500

    Initial commit

---
+++ lib/Alien/Build/Plugin/Fetch/FreeBSDFetch.pm
@@ -0,0 +1,10 @@
+package Alien::Build::Plugin::Fetch::FreeBSDFetch;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Alien::Build plugin for FreeBSD's fetch
+# VERSION
+
+1;
---
+++ t/alien_build_plugin_fetch_freebsdfetch.t
@@ -0,0 +1,6 @@
+use Test2::V0 -no_srand => 1;
+use Alien::Build::Plugin::Fetch::FreeBSDFetch;
+
+ok 1, 'todo';
+
+done_testing
